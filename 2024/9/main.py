8250e11c00902c82a8f46cedfe07c80868efbed9
Nhan Nguyen <nhnguyen@ualberta.ca>
2024-12-09 16:55:42 -0700

    day 9 part 2

--- 2024/9/main.py
+++ 2024/9/main.py
@@ -28,7 +28,69 @@ def one(display):
     checksum = calculate_checksum(display)
     
     return checksum
+
+def two(display):
+    group_display = []
+    key_to_count = {}
+
+    current = display[0]
+    count = 1
     
+    
+    for i in range(1, len(display)):
+        if display[i] == current:
+            count+=1
+        else:
+            group_display.append((current, count))
+            if current != '.':
+                key_to_count[current] = count 
+            
+            current = display[i]
+            count = 1
+
+    group_display.append((current, count))
+    key_to_count[current] = count 
+    max_number = int(max(key_to_count.keys()))
+
+    for num in range(max_number, 0, -1):
+        num = str(num)
+        num_len = key_to_count[num]
+        num_pos = group_display.index((num, num_len))
+        
+        for index, values in enumerate(group_display[:num_pos]):
+            key, length = values
+            
+            if key == '.' and length >= num_len:
+                group_display[num_pos] = ('.', num_len)
+                group_display[index] = (num, num_len)
+
+                remain_num = length - num_len
+                
+                if remain_num > 0:
+                    group_display.insert(index+1, ('.', remain_num))
+                    
+                break
+    
+    
+    output = ''
+    for element, count in group_display:
+        output+= element*count
+    
+
+    total = 0
+    position = 0
+
+    for item, size in group_display:
+        if item is '.':
+            position += size
+        else:
+            for n in range(size):
+                total += int(item) * position
+                position += 1
+    
+    print(total)
+    
+
 def main():
     filename = sys.argv[1]
     display = []
@@ -47,24 +109,7 @@ def main():
                     display.append('.')
         
     # print(one(display))
-    
-    print(display)
-
-    end = len(display)-1
-    for start in range(len(display)-1,-1,-1):        
-        if display[start] != display[end]:            
-            if display[end] != '.':
-                # move display[start+1:end+1] to furthest left same-length of contiguous '.' 
-                print(display[start+1:end+1])    
-
-            # start new
-            end = start
-        
-
-    checksum = calculate_checksum(display)
-    
-    return checksum
-
+    # print(two(display))
 
 if __name__ == '__main__':
     main()
